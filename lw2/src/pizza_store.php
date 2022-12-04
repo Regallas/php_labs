ded8668cd187c6d5817f7bc46f762214891838bf
Regallas <uhbap@mail.ru>
2022-12-04 15:06:54 +0300

    feat(lw2): first commit

---
+++ lw2/src/pizza_store.php
@@ -0,0 +1,14 @@
+<?php
+require_once "pizza.php";
+abstract class PizzaStore
+{
+    abstract function createPizza(string $type): Pizza;
+
+    public function orderPizza(string $type): void
+    {
+        print ("Приветствуем в нашей пиццерии, какие будут пожелания?") . PHP_EOL;
+        $pizza = $this->createPizza($type);
+        $pizza->prepare();
+        $pizza->cut();
+    }
+}
---
+++ lw2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '1/lw2',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '56414207a5e189f3763ada170d197bd3be21a9ca',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '1/lw2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '56414207a5e189f3763ada170d197bd3be21a9ca',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.9.2',
+            'version' => '1.9.2.0',
+            'reference' => 'd6fdf01c53978b6429f1393ba4afeca39cc68afa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
